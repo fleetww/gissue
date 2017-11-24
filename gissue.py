b97f04f8f2e437a656e42cdcc8a8ecfb2e8b6bf8
fleetww <fleetww@wwu.edu>
2017-11-23 17:57:44 -0800

    Making progress on arg parsing

--- gissue.py
+++ gissue.py
@@ -5,20 +5,40 @@ import sys, argparse
 
 def build_url(args):
     url = "https://api.github.com/repos/"+args.owner[0]+"/"+args.repo[0]+"/issues"
+    #If user requested a single issue number, no reason to filter anything else
+    if (args.number != None):
+        url += "/" + args.number[0]
+        return url
+    url += "?"
+    if (args.state != None):
+        url += '&state=' + args.state[0]
     return url
 
-#Getting user, repo, desired issue statuses
-argparser = argparse.ArgumentParser(description="Retrieve issues for a given git repo on either github or gitlab.")
-argparser.add_argument("-r", "--repo", nargs=1, required=True)
-argparser.add_argument("-o", "--owner", nargs=1, required=True)
-argparser.add_argument("-s", "--state", nargs='+')
+def build_arg_parser():
+    argparser = argparse.ArgumentParser(description="Retrieve issues for a given git repo on either github or gitlab.")
+    argparser.add_argument("-r", "--repo", nargs=1, required=True)
+    argparser.add_argument("-o", "--owner", nargs=1, required=True)
+    argparser.add_argument("-s", "--state", nargs=1)
+    argparser.add_argument("-n", "--number", nargs=1)
 
-args = argparser.parse_args()
+    args = argparser.parse_args()
 
+    return args
+
+
+args = build_arg_parser()
 
 url = build_url(args)
+print(url)
 
-response = urllib.request.urlopen(url)
-data = json.loads(response.read().decode())[0]
+try:
+    response = urllib.request.urlopen(url)
+except:
+    print("Could not find the desired issues for that repo, sorry")
+    exit(0)
+
+#Will only be a single issue if number specified, so in the future we must check
+#if the desired number was specified
+data = json.loads(response.read().decode())
 print(data)
 
